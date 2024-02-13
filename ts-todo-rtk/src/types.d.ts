d546d52598cb0e7e9c3512d782050de4e147dc04
hasan <hasancilenger1773@gmail.com>
2024-02-13 22:59:38 +0100

    rtk

---
+++ ts-todo-rtk/src/types.d.ts
@@ -0,0 +1,10 @@
+//! .d.ts uzantısnı typescript global alan olarak görüyor. Vu bu uzantıdaki dosyalarda tanımladığımız typelara herhangi bir export-import yapmadan erişim sağlayabiliyoruz.
+
+interface ITodoType {
+  todo: string;
+  isDone: boolean;
+  id: string | number;
+  task?: string; //!optional
+}
+
+
