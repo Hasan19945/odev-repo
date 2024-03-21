dbefac803bf1695675467d199db0fa8a0360d8a6
hasan <hasancilenger1773@gmail.com>
2024-03-21 14:57:57 +0100

    handleChange

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,22 @@
-import BirinciComponents from "./Components/BirinciComponents";
-import Deneme from "./Components/Deneme";
+import { useState } from 'react';
 
-function App() {
+ function MyInput() {
+  const [text, setText] = useState('hei');
 
-const deneme = "props mantigi";
-const arr =[
-  {name:'array1', id:0},
-  {name:'array2', id:1},
-  {name:'array3', id:2}
-]
- console.log(arr, "arr");
-const tiklama = () => {
-  console.log("butona tikla")
-
-   }
+  function handleChange(e) { setText(e.target.value)
+  }
 
   return (
-    <div className="App">
-      <div>deneme</div>
-      <div>yeni bir {deneme}</div>
-      <BirinciComponents props={deneme}/>
-      <Deneme/>
-      <button onClick={tiklama}>tikla</button>
-
-   {
-    arr.map((a) => (
-      <div key={a.id}>{a.name}</div>
-    ))
-
-   }
-     
-
+    <>
+      <input type='text' value={text} onChange={handleChange} />
+      <p> Yazilan metin {text}</p>
+     <button onClick={() => setText('Merhaba')}> Sifirla</button>
     
-    </div>
+    </>
   );
 }
 
-export default App;
+
+
+
+export default MyInput
